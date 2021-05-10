b45cdde5083b80c6d1bd7feca5f44bac51973264
root <root@DESKTOP-QPK7AHB.localdomainzoo8100_@naver.com>
2021-05-10 18:00:25 +0900

    로그인을 요청하는 fetch 생성

--- login-page/src/public/js/home/login.js
+++ login-page/src/public/js/home/login.js
@@ -4,14 +4,23 @@ const id = document.querySelector("#id"),
 password = document.querySelector("#password"),
 loginBtn = document.querySelector("button")
 
-loginBtn.addEventListener("click", login());
+loginBtn.addEventListener("click", login);
 
 function login() {
   const req = {
       id: id.value,
       password: password.value,
   };
-console.log(req); 
+
+  console.log(req);
+  console.log(JSON.stringify(req));
+fetch("/login", {
+    method:"POST",
+    header:{
+        "Content-Type":"application/json"
+    },
+    body: JSON.stringify(req),
+});
 }
 
  
--- login-page/src/routes/home/index.js
+++ login-page/src/routes/home/index.js
@@ -8,5 +8,6 @@ const ctrl = require("./home.ctrl");
 router.get("/", ctrl.hello);
 
 router.get("/login",ctrl.login);
+// router.post("/login", ctrl.login);
 
 module.exports = router;
