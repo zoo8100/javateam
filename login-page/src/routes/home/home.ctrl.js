a001c50df15ce52939330201b1f110d6d2f320e4
root <root@DESKTOP-QPK7AHB.localdomainzoo8100_@naver.com>
2021-05-11 04:26:22 +0900

    User 모델 만들기

--- login-page/src/routes/home/home.ctrl.js
+++ login-page/src/routes/home/home.ctrl.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
+// const UserStorage = require("../../models/UserStorage");
 
 const output={
 
@@ -12,26 +13,12 @@ const output={
     },
 }
 
-
-
 const process = {
     login: (req, res) => {
-        const id = req.body.id,
-        password = req.body.password;
-
-    const users =  UserStorage.getUsers("id","password");
-
-        const response ={};
-        if (users.id.includes(id)) {
-            const idx =users.id.indexOf(id);
-         if (users.password[idx] === password){
-                response.success = true;
-                return res.json(response);
-            }
-        }
-        response.success =false;
-        response.msg = "로그인에 실패하였습니다."
-        return res.json(response);
+       const user = new User(req.body);
+       const response = user.login();
+       console.log(response);
+      return res.json(response);
     },
 };
 
